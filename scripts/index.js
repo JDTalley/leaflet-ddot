6339702703d8c80d12935d390e99676ba544f5bd
Dakota Talley <dakota.talley@gmail.com>
2019-12-01 21:24:40 -0500

    Added Script and Style

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+var mymap = L.map('mapid').setView([38.9072, -77.0369], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg'
+}).addTo(mymap);
+
+L.esri.featureLayer(
+    {
+        url: 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DDOT/DdotAssets/MapServer/13'
+    }).addTo(mymap);
+
+/* var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle");
+polygon.bindPopup("I am a polygon");
+
+ */
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
