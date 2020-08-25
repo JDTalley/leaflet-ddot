c5be3cb6d175f0d8c17b314ab34951981459c6a7
Dakota <dakota.talley@gmail.com>
2020-08-24 21:41:42 -0400

    Added Cityworks Work Orders.

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+// Wards
 L.esri.featureLayer(
     {
         url: 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DDOT/DdotAssets/MapServer/13'
     }).addTo(mymap);
 
-/* var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle");
-polygon.bindPopup("I am a polygon");
-
- */
+// Work Orders
+L.esri.featureLayer(
+    {
+        url: 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DDOT/Cityworks/MapServer/0'
+    }).addTo(mymap);
 
 var popup = L.popup();
 
